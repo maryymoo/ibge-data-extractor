9332ca72efdbfd8920f42e5799c8bc230d9e1a61
Maria Moreira <maria.m.cardoso56@gmail.com>
2024-11-15 11:14:20 +0000

    Testing integration with Selenium in the Extractor class

--- src/extractor.py
+++ src/extractor.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 
 class Extractor:
     def __init__(self):
@@ -14,10 +15,13 @@ class Extractor:
         return driver
 
     def download_data(self, url):
-        self.driver.get(url)
-        # A lógica para encontrar e clicar no botão de download
-        download_button = self.driver.find_element_by_xpath("//button[@class='download']")
-        download_button.click()
+        try:
+            self.driver.get(url)
+            # Logic to find and click on the download button
+            download_button = self.driver.find_element_by_xpath("//button[@class='download']")
+            download_button.click()
+        except NoSuchElementException:
+            print("Error: Download button not found.")
 
     def close_driver(self):
         self.driver.quit()
